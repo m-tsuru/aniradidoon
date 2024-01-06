5ebd2b20ddf8b1a39bf9d32c359a4fec49d1893a
m_tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-01-06 19:51:53 +0900

    Dev: Implement of Quit(Q)

--- src/aniradidoon/main.py
+++ src/aniradidoon/main.py
@@ -1,15 +1,19 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer
+import sys
 
 class aniradidoonApp(App):
-    BINDINGS = [("d", "toggle_dark", "Dark Mode")]
+    BINDINGS = [("d", "toggleDark", "Dark"), ("q", "quit", "Quit")]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
 
-    def action_toggle_dark(self) -> None:
+    def action_toggleDark(self) -> None:
         self.dark = not self.dark
+
+    def action_quit(self) -> None:
+        sys.exit()
 
 if __name__ == "__main__":
     app = aniradidoonApp()
