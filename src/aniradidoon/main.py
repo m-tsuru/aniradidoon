e55280e8930b664d7f28bfa36db6d772a1e3473f
m_tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-01-06 20:40:23 +0900

    Dev: Window Tabs

--- src/aniradidoon/main.py
+++ src/aniradidoon/main.py
@@ -1,13 +1,22 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer
+from textual.widgets import Header, Footer, Tabs
 import sys
+
+Channels = [
+    "Onsen",
+    "Settings"
+]
 
 class aniradidoonApp(App):
     BINDINGS = [("d", "toggleDark", "Dark"), ("q", "quit", "Quit")]
 
     def compose(self) -> ComposeResult:
-        yield Header()
+        yield Header(show_clock=True, name="AniradiDoon - Control")
+        yield Tabs(Channels[0], Channels[1])
         yield Footer()
+
+    def on_mount(self) -> None:
+        self.query_one(Tabs).focus()
 
     def action_toggleDark(self) -> None:
         self.dark = not self.dark
