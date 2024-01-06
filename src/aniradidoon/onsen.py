c688f64e73bdb71da64a90a902fac7fdcf9d0940
m_tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-01-06 19:45:39 +0900

    Format: onsen.py

--- src/aniradidoon/onsen.py
+++ src/aniradidoon/onsen.py
@@ -6,25 +6,29 @@ baseURI = "https://www.onsen.ag/"
 header = {"content-type": "application/json"}
 maximumTryCount = 3
 
-class OnsenAPIData():
+
+class OnsenAPIData:
     # Get program information from onsen API
     def __init__(self):
         for tryCount in range(0, maximumTryCount):
             try:
                 self.result = True
-                self.rawData = requests.get(baseURI + "web_api/programs", headers=header).json()
+                self.rawData = requests.get(
+                    baseURI + "web_api/programs", headers=header
+                ).json()
                 break
             except Exception as e:
                 if tryCount > maximumTryCount:
                     self.result = False
                     self.rawData = ""
                     raise e
-                else: 
+                else:
                     print(f"Error: Trial Count: {tryCount}")
                     print(f"{e}")
-                    continue   
+                    continue
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     data = OnsenAPIData()
     if data.result:
         print("データの取得に成功しています。↓")
