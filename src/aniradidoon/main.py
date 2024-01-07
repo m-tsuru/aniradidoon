5f2996f9cfe56d800bfd1be8805515e0d7c837c8
m_tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-01-08 00:23:31 +0900

    Dev: Get and List Summary of Onsen Programs

--- src/aniradidoon/main.py
+++ src/aniradidoon/main.py
@@ -1,20 +1,58 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Tabs
+from textual.screen import Screen
+from textual.widgets import Header, Footer, Tabs, DataTable, Placeholder
 import sys
+import onsen
 
-Channels = ["Onsen", "Settings"]
-
+onsenBaseData = onsen.OnsenAPIData()
 
 class aniradidoonApp(App):
     BINDINGS = [("d", "toggleDark", "Dark"), ("q", "quit", "Quit")]
+    class onsenListScreen(Screen):
 
-    def compose(self) -> ComposeResult:
-        yield Header(show_clock=True, name="AniradiDoon - Control")
-        yield Tabs(Channels[0], Channels[1])
-        yield Footer()
+        def compose(self) -> ComposeResult:
+            self.success = True
+            yield Header(show_clock=True, name="AniradiDoon - Control")
+            yield Tabs("Onsen")
+            if onsenBaseData.result == True:
+                try:
+                    yield DataTable()
+                except Exception as e:
+                    yield Placeholder(f"Summary Could not be got: {e}")
+                    self.success = False
+            else:
+                yield Placeholder(f"{onsenBaseData.error}")
+                self.success = False
+            yield Footer()
+            
+        def on_mount(self) -> None:
+            if self.success != False:
+                summary = onsenBaseData.summary()
+                table = self.query_one(DataTable)
+                table.cursor_type = "row"
+                table.zebra_stripes = True
+                table.add_columns(*summary[0])
+                table.add_rows(summary[1:])
+
+    class preferencesScreen(Screen):
+        def compose(self) -> ComposeResult:
+            yield Header(show_clock=True, name="AniradiDoon - Control")
+            yield Placeholder("Preferences")
+            yield Footer()
+
+    MODES = {
+        "Onsen": onsenListScreen,
+        "Download": downloadScreen,
+        "Preferences": preferencesScreen,
+    }
+
+    BINDINGS.append(("p", "switch_mode('Preferences')", "Preferences"))
 
     def on_mount(self) -> None:
-        self.query_one(Tabs).focus()
+        self.switch_mode("Onsen")
+
+    def on_data_table_row_selected(self):
+        self.switch_mode('Download')
 
     def action_toggleDark(self) -> None:
         self.dark = not self.dark
--- src/aniradidoon/onsen.py
+++ src/aniradidoon/onsen.py
@@ -10,9 +10,9 @@ maximumTryCount = 3
 class OnsenAPIData:
     # Get program information from onsen API
     def __init__(self):
+        self.result = True
         for tryCount in range(0, maximumTryCount):
             try:
-                self.result = True
                 self.rawData = requests.get(
                     baseURI + "web_api/programs", headers=header
                 ).json()
@@ -20,6 +20,7 @@ class OnsenAPIData:
             except Exception as e:
                 if tryCount > maximumTryCount:
                     self.result = False
+                    self.error = "Could not get from onsen.ag/web_api/programs : " + e
                     self.rawData = ""
                     raise e
                 else:
@@ -27,11 +28,27 @@ class OnsenAPIData:
                     print(f"{e}")
                     continue
 
+    def summary(self):
+        self.summary = []
+        try:
+            self.summary.append(("latest_date", "directory_name", "title", "latest_count"))
+            for i in range(len(self.rawData)):
+                program = self.rawData[i]
+                self.summary.append((program["title"],
+                                    program["updated"],
+                                    program["directory_name"],
+                                    "No Data" if not program["contents"] else program["contents"][0]["title"]))
+            return self.summary
+        except Exception as e:
+            self.summary = []
+            self.result = False
+            self.error = "Could not generate summary : " + e
+
 
 if __name__ == "__main__":
     data = OnsenAPIData()
     if data.result:
         print("データの取得に成功しています。↓")
-        print(json.dumps(data.rawData))
+        print(data.summary()) 
     else:
         print("データの取得に失敗しています。")
